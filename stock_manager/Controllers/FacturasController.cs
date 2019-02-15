4b20e6947fdc17fa7baed7927e3c8954eda6aefe
andarms <andarms@outlook.com>
2019-02-15 11:40:37 -0500

    Crud Items

---
+++ stock_manager/Controllers/FacturasController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using stock_manager.Models;
+
+namespace stock_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturasController : ControllerBase
+    {
+        private readonly BaseDatosContext _context;
+
+        public FacturasController(BaseDatosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Facturas
+        [HttpGet]
+        public IEnumerable<Facturas> GetFacturas()
+        {
+            return _context.Facturas;
+        }
+
+        // GET: api/Facturas/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFacturas([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var facturas = await _context.Facturas.FindAsync(id);
+
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(facturas);
+        }
+
+        // PUT: api/Facturas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFacturas([FromRoute] int id, [FromBody] Facturas facturas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != facturas.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(facturas).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacturasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Facturas
+        [HttpPost]
+        public async Task<IActionResult> PostFacturas([FromBody] Facturas facturas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Facturas.Add(facturas);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFacturas", new { id = facturas.Id }, facturas);
+        }
+
+        // DELETE: api/Facturas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFacturas([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var facturas = await _context.Facturas.FindAsync(id);
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Facturas.Remove(facturas);
+            await _context.SaveChangesAsync();
+
+            return Ok(facturas);
+        }
+
+        private bool FacturasExists(int id)
+        {
+            return _context.Facturas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ stock_manager/Controllers/ItemsController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using stock_manager.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stock_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly BaseDatosContext _context;
+
+        public ItemsController(BaseDatosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        public IEnumerable<Items> GetItems()
+        {
+            return _context.Items;
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItems([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var items = await _context.Items.FindAsync(id);
+
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(items);
+        }
+
+        // PUT: api/Items/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItems([FromRoute] int id, [FromBody] Items items)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != items.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(items).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Items
+        [HttpPost]
+        public async Task<IActionResult> PostItems([FromBody] Items item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            /*
+             * Busca si la medida ya esta ingresada dentro del sistema, s i no es así agrega la nueva unidad de medida.
+             * Todos los nombre de medidas van en minusculas.
+             */
+            var nomMedida = item.Medida.Nombre.ToLower();
+            Medidas medida = _context.Medidas.FirstOrDefault(m => m.Nombre == nomMedida);
+            if (medida == null)
+            {
+                medida = new Medidas { Nombre = nomMedida };
+                _context.Medidas.Add(medida);
+            }
+            item.Id_Medida = medida.Id;
+            item.Medida = medida;
+
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItems", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/Items/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItems([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var items = await _context.Items.FindAsync(id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(items);
+            await _context.SaveChangesAsync();
+
+            return Ok(items);
+        }
+
+        private bool ItemsExists(int id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
